b789c0cbd15f51a4da86965144381bd0db756d43
timothy Tamm <tamm@college.harvard.edu>
2018-02-24 13:47:04 -0500

    add ms key

--- models/config.js
+++ models/config.js
@@ -2,6 +2,6 @@ module.exports = {
   port: process.env.PORT || 5000,
   dbUrl: process.env.DB_URL || 'localhost',
   msEnd: 'eastus.api.cognitive.microsoft.com',
-  msKey: 'todo',
+  msKey: process.env.MS_KEY,
   faceListId: 'fitJet-1918',
 }
